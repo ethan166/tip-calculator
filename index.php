209bc857d4bfaf4c17ded5898beeb65de8f66b71
Ethan Chen <ktun7@mail.ccsf.edu>
2017-01-03 11:25:17 -0800

    Added the split_total feature

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	$percentages = array(0.1, 0.15, 0.2, "custom");
 	$wrongInput = false;
 	$wrong_tip_input = false;
+	$wrong_split_input = false;
 
 	if($_SERVER["REQUEST_METHOD"] == "POST")
 	{
@@ -11,6 +12,7 @@
 		$bill = $_POST["bill"];
 		$tip_percent = $_POST["tip_percent"];
 		$custom_tip = $_POST["custom"];
+		$split_bill = $_POST["split_bill"];
 
 		// checking input text number validation
 		if($bill == "" OR !is_numeric($bill) OR $bill < 0) {
@@ -25,6 +27,13 @@
 			}
 		}
 
+		if(isset($split_bill))
+		{
+			if($split_bill == "" OR !is_numeric($split_bill) OR $split_bill <= 0) {
+				$wrong_split_input = true;
+			} 
+		}
+
 		// converting the percentages into decimals for custom tip
 		if($tip_percent == "custom" && isset($custom_tip) && $wrong_tip_input == false)
 		{
@@ -33,7 +42,7 @@
 		}
 
 		// tip calculation 
-		if($wrongInput == false && $wrong_tip_input == false)
+		if($wrongInput == false && $wrong_tip_input == false && $wrong_split_input == false)
 		{	
 
 			if(isset($calculated_custom_tip))
@@ -44,6 +53,7 @@
 			}	
 		}
 
+		
 		// showing the output
 		if(isset($calculated_tip)) {
 			$total = $calculated_tip + $bill;
@@ -51,6 +61,16 @@
 		$output ="";
 		$output .= "Tip: $" . $calculated_tip ."\n";
 		$output .= "Total: $" . $total;
+
+		// splitting the tip and the total
+		if(isset($split_bill) && $split_bill > 1 && $wrong_split_input == false)
+		{
+			$each_tip = $calculated_tip / $split_bill;
+			$each_total = $total / $split_bill;
+			$output .= "\nTip each: $ " . $each_tip;
+			$output .= "\nTotal each: $" . $each_total;
+		}
+		
 		}
 		
 	}
@@ -115,6 +135,7 @@
 		input[type=text] {
 			width: 80px;
 		}
+
 		.error_input
 		{
 			border-color:red;
@@ -132,7 +153,7 @@
 			<div class="myborder">
 			<h2>Tip Calculator</h2>
 			<br>
-
+				<!-- beginning of form -->
 				<form method="post" action="index.php">
 					<label  <?php if($wrongInput) echo "class='error_input'" ?> for="bill">Bill subtotal $:</label>
 					<input  <?php if($wrongInput) echo "class='error_input'" ?> type="text" id="bill" name="bill" value=<?php if (isset($bill)) echo $bill;?> >
@@ -140,24 +161,24 @@
 					<br><br>
 					<label <?php if($wrong_tip_input) echo "class='error_input'" ?> for="tip_percent">Tip percentage : </label><br>
 					<?php for($i = 0; $i < 4; $i++) { ?>
-					 <label <?php if($wrong_tip_input) echo "class='error_input'" ?> ><br><input <?php if($wrong_tip_input) echo "class='error_input'" ?> type="radio" name="tip_percent" <?php if (isset($tip_percent) && $tip_percent==$percentages[$i]) { echo "checked"; } ?> value=<?php echo $percentages[$i]; ?> />
-					 <?php 
-					 	
-					 	if($i == 3)
-					 	{ ?>
+					 	<label <?php if($wrong_tip_input) echo "class='error_input'" ?> ><br><input <?php if($wrong_tip_input) echo "class='error_input'" ?> type="radio" name="tip_percent" <?php if (isset($tip_percent) && $tip_percent==$percentages[$i]) { echo "checked"; } ?> value=<?php echo $percentages[$i]; ?> />
+						 
+						<?php if($i == 3) { ?>
 					 	  <label <?php if($wrong_tip_input) echo "class='error_input'" ?> for='custom'><?php  echo $percent_words[$i];  ?>
 					 	  <input <?php if($wrong_tip_input) echo "class='error_input'" ?> type='text' id='custom' name='custom' value=<?php if (isset($custom_tip)) echo $custom_tip; ?> > % </label>
 					 	
 					 	<?php  } else { 
 					 		echo $percent_words[$i];
-					      } ?>
-
-					  
+					      } ?>  
 					</label>
 					<?php } ?>
+					<br><br>
+					<label <?php if($wrong_split_input) echo "class='error_input'" ?> for="split_bill">Split: <input type='text' name="split_bill" value=<?php if (isset($split_bill)) echo $split_bill; else echo "1"; ?> > person(s)</label>
 
 					<div class="button_box"><button type="submit" />Submit</button></div>	
-					</form>
+				</form>
+				<!-- end of form -->
+
 				<!-- output div -->
 				<?php if(isset($calculated_tip)) { ?>
 				<div class="tip_output"> <pre><?php if (isset($output)) {echo $output;} ?></pre></div>
